05e8a7966704f61dd2f051ce404a26159deadf93
vikash yadav <yadav621gzp@gmail.com>
2022-07-05 23:25:26 +0530

    all the file added to the final project

---
+++ Development/ChainLink/Task01/index.js
@@ -0,0 +1,24 @@
+if(typeof window.ethereum =="undefined"){
+    console.log("PLease install the metamask");
+}
+
+$("document").ready(function (){
+async function interact(){
+
+const web3 = await  new Web3("https://rinkeby.infura.io/v3/b208399f926f487093f45debc86299bb")
+const aggregatorV3InterfaceABI = await [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
+const addr = await "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"
+const priceFeed = await new web3.eth.Contract(aggregatorV3InterfaceABI, addr)
+
+
+   $("#btn2").click(async function (){
+  const data = await priceFeed.methods.latestRoundData().call();
+  console.log(data[1]);
+  alert(data[1]);
+})
+
+
+
+}
+interact();
+})
